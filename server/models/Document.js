96b1a25face0184a57c1b463ac84b6e7f04ce535
Tyler Htet Naing Phyo <42888726+TylerHNP@users.noreply.github.com>
2022-05-03 04:19:07 -0400

    fixed document error

--- server/models/Document.js
+++ server/models/Document.js
@@ -1,10 +1,12 @@
-const { Schema, model } = require("mongoose")
-const { mongooseConnection } = require('../db/connectDB');
+const { Schema, model } = require("mongoose");
+const { mongooseConnection } = require("../db/connectDB");
 const docSchema = new Schema({
     _id: String,
-    name: String,
-    content: Object,
-})
+    ops: [],
+    _type: String,
+    _v: Number,
+    _m: { ctime: Number, mtime: Number },
+});
 
-const Document = mongooseConnection.model("Document", docSchema)
+const Document = mongooseConnection.model("Documents", docSchema);
 exports.Document = Document;
